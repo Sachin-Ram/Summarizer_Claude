0bdd3ab985d7fdc9145202922f50163e4d97c3f2
sachin <sachinram2510@gmail.com>
2024-12-12 20:50:56 +0530

    claude api tested

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
 from flask import Flask,request,jsonify
 import os
+from dotenv import load_dotenv 
+load_dotenv()
 import anthropic
 """
 1.get the api key
@@ -10,12 +12,29 @@ import anthropic
 """
 app=Flask(__name__)
 
-@app.route("/")
+api_key=os.getenv('api_key')
+
+client=anthropic.Anthropic(api_key=api_key)
+
+@app.route("/",methods=['GET','POST'])
 def function():
-    data=""
-    return data
+    data=request.form.get('data')
+    text=client.messages.create(
+        model="Claude 3.5 Sonnet 2024-10-22",
+        max_tokens=300,
+        system="You are a text summarizer and when asked to summarize a text, send back the summary",
+        messages=[
+            {
+                'role':"user",
+                'content':f"summarize this text : {data}"
+            }
+        ]
+    )
 
+    summary = text.get("content")
 
-if __name__ =='main':
+    return summary
+
+if __name__ =="__main__":
      
     app.run(debug=True)
